dbe2bce765af556ae7563940f1be760120a04650
mohammadamin jafari <m.a.jafari002@gmail.com>
2023-09-10 18:52:15 +0430

    Styles more flexible

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ let menuIcon = document.querySelector('#menuicon');
 let menuIconSpan = document.querySelector('.menu-icon');
 let menuListsm = document.querySelector('.menu-list-sm');
 let open = false;
+const twTextEl = document.querySelector('#tw-text');
 menuIcon.addEventListener('click', function () {
       menuIconSpan.classList.toggle('menu-icon--open');
       open = !open;
@@ -15,4 +16,24 @@ menuIcon.addEventListener('click', function () {
             menuListsm.classList.remove('close');
             menuListsm.classList.add('open');
       }, 200);
-})
+});
+
+function twHandler() {
+      const twElement = new Typewriter(twTextEl, {
+            loop: true,
+            cursor: ''
+      }).typeString("ترکیب هنر")
+            .pauseFor(800)
+            .deleteChars(3)
+            .typeString("تکنولوژی")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("تجربه منحصر به فرد در زمینه وب")
+            .pauseFor(800)
+            .deleteChars(2)
+            .typeString("هاستینگ")
+            .pauseFor(1000)
+            .start();
+}
+
+window.addEventListener('load', twHandler);
